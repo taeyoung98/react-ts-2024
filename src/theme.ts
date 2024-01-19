18cfb8e870415786d15d4e47a27739d380b2bb7d
tang <119taeyoung@naver.com>
2024-01-19 18:07:11 +0900

    theme

---
+++ src/theme.ts
@@ -0,0 +1,14 @@
+import { DefaultTheme } from "styled-components"
+
+
+export const lightTheme: DefaultTheme = {
+  bgColor: '#11235A',
+  textColor: '#F6ECA9', // #F6ECA9 #C6CF9B
+  accentColor: '#596FB7',
+}
+
+export const darkTheme: DefaultTheme = {
+  bgColor: '#111',
+  textColor: 'whitesmoke',
+  accentColor: 'teal'
+} 
